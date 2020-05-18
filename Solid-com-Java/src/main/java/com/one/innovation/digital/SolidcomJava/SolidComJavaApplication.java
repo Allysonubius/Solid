cb8f85c3bca086ca53e76987c39b332e94927de4
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-18 08:36:22 -0300

    Iniciado projeto Solid estudando.

---
+++ Solid-com-Java/src/main/java/com/one/innovation/digital/SolidcomJava/SolidComJavaApplication.java
@@ -0,0 +1,13 @@
+package com.one.innovation.digital.SolidcomJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidComJavaApplication.class, args);
+	}
+
+}
---
+++ Solid-com-Java/src/test/java/com/one/innovation/digital/SolidcomJava/SolidComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.innovation.digital.SolidcomJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
